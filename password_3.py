81f5abd5bcada6328dfe963f54e96e19a661042b
Sam Pan <pwl12344321@gmail.com>
2022-07-12 00:55:24 +0800

    change to while i > 0

--- password_3.py
+++ password_3.py
@@ -5,12 +5,12 @@
 # 對的話 印出'Correct and enter now'
 
 password = 'a123456'
-i = 2
-while i >= 0:
+i = 3
+while i > 0:
 	x = input('please enter your password: ')
 	if x == password:
 		print('Correct and enter now')
 		break
 	else:
-		print('Wrong! You have', i,' chances left')
 		i = i - 1
+		print('Wrong! You have', i,' chances left')
